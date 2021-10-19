813dab40dddbd1001cfd9853b8b7aa44684f44fc
Abdulloh <abdullohjonirgashev@gmail.com>
2021-10-19 09:23:18 +0500

    intro to javasript

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log('js loaded from outline file');const number = '+998901826858';{
+    const number = 78787; 
+     console.log(number);
+}
+    let x = 5 //Reassign can be
+    x = 8;
+    const y = 10 ; //can not be reassign 
+    y = 11
+
+console.log(number);
